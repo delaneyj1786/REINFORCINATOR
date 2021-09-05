c511e4d46d21045d5b295d850c09f66b85ad60d7
James <delaneyj@pdx.edu>
2021-09-05 16:53:55 -0700

    replaced with myfun3 with group_split_recounter

--- R/funs.R
+++ R/funs.R
@@ -316,7 +316,14 @@ group_splitter <- function(data, behaviorstream, behavior,consequence,
 
 }
 
-myfun3 <-function(a,b,c) {tibble(a,b,c)}
+group_split_recounter<- function(list, behaviorstream, behavior,consequence,actor){
+  behaviorstream
+  list %>% map(~Recounter5(.x,behaviorstream,behavior,consequence,actor)$recounted_data_frame) %>%
+    map_dfr(~as.data.frame(.), .id = "GROUP")
+
+  # ## TESTING
+  # group_split_recounter(gs_test,behaviorstream = "BEH","x","A",actor = "TAR")
+}
 
 
 
