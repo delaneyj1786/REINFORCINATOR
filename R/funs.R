8bcf7e3da320ba88d5e84aef36feed7b3d1fb693
James <delaneyj@pdx.edu>
2021-09-04 18:13:13 -0700

    -added elevator dataset -added documentation for elevator -added basic description

--- R/funs.R
+++ R/funs.R
@@ -3,3 +3,19 @@ myfun <- function(x) {x+1}
 myfun2 <-function(x,y) { (x+y) / y}
 
 myfun3 <-function(a,b,c) {tibble(a,b,c)}
+
+
+
+#################
+## DATASETS #############
+elevator<-tibble(
+  VIDELT = rep(2,14),
+  TAR = rep("S",14),
+  BEH = c("o","x",
+          "x","x",
+          "A",
+          "o","x","x","x",
+          "A",
+          "o","o","o","x"),
+  LAG_BEH = lag(BEH)
+)
