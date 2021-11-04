3653b790ad23a2e4c2b19d6079b4edee405dfc5a
James <delaneyj@pdx.edu>
2021-11-04 15:17:46 -0700

    removed group_split recounter 2 (since i basically changed it anyway)

--- R/funs.R
+++ R/funs.R
@@ -644,14 +644,6 @@ group_split_recounter<- function(list, behaviorstream, behavior,consequence,acto
 }
 
 
-group_split_recounter2 <- function(list, behaviorstream, behavior,consequence,actor){
-  list %>% map(~Recounter5(.x,behaviorstream,behavior,consequence,actor)$recounted_data_frame) %>%
-    map_dfr(~as.data.frame(.), .id = "GROUP")
-
-  # ## TESTING
-  # group_split_recounter(gs_test,behaviorstream = "BEH","x","A",actor = "TAR")
-}
-
 ###################
 ### Reformatting Functions ####
 
